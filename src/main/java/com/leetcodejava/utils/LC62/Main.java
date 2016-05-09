7ef8906becc22c8deabd4236feeeca7d642f88d9
DehaoWang <evermore4@163.com>
2016-05-09 15:44:21 +0800

    compare

--- src/main/java/com/leetcodejava/utils/LC62/Main.java
+++ src/main/java/com/leetcodejava/utils/LC62/Main.java
@@ -1,17 +1,26 @@
 package com.leetcodejava.utils.LC62;
 
+import java.util.Timer;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 19;
+		for(int n = 0; n < i; n++) {
+			long timedpmemo1 = System.currentTimeMillis();
+			Memo testM = new Memo(n, n);
+			System.out.print(testM.dfs(n, n)+" ");
+			long timedpmemo2 = System.currentTimeMillis();
+			System.out.println("MEMO DP Time used: " + (timedpmemo2 - timedpmemo1));
 
-//		System.out.println(uniquePath(30, 30));
-		
-		Memo testM = new Memo(10, 10);
-		
-		System.out.println(testM.dfs(10, 10));
-		
-		testM.printMatrix();
+
+			//testM.printMatrix();
+			long timedp1 = System.currentTimeMillis();
+			System.out.print(uniquePath(n, n)+" ");
+			long timedp2 = System.currentTimeMillis();
+			System.out.println("DP Time used: " + (timedp2 - timedp1));
+		}
 	}
 
 	public static int uniquePath(int m, int n)
